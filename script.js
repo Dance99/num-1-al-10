032fd97568d1494f43f154c344d39a9140e57673
Daniela <nam991019@gmail.com>
2024-11-03 23:57:45 -0500

    bucle alert

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 let numero;
+let valido = false; 
 
-let Valido = falso;
+while (!valido) {
+    numero = parseInt(prompt("Ingresa un número del 1 al 10: ")); 
 
-while (!Valido) {
-    numero = parseInt=(prompt("Ingresa un numero del 1 al 10: "));
+    switch (numero) { 
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+        case 7:
+        case 8:
+        case 9:
+        case 10:
 
-switch ("numero") {
-    case 1:
-    case 2:
-    case 3:
-    case 4:
-    case 5:
-    case 6:
-    case 7:
-    case 8:
-    case 9:
-    case 10:
-    valido = true;
-         break;
-    
-         default:
-         alert("Ingresa el numero el numero valido entre 1 y 10.")
-         break;
-}
-}
- 
+            valido = true; 
+            break;
+            
+        default:
+            alert("Por favor, ingresa un número válido entre 1 al 10.");
+            break;
+    }
+} 
+
 alert("Gracias");
