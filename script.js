87f21cc83209d602e477370f924eb3a474d02483
Daniela <nam991019@gmail.com>
2024-11-03 22:59:53 -0500

    case del 1 al 10

---
+++ script.js
@@ -0,0 +1,24 @@
+let numero;
+
+let Valido = falso;
+
+while (!Valido) {
+    numero = parseInt=(prompt("Ingresa un numero del 1 al 10: "));
+
+switch ("numero") {
+    case "1":
+    case "2":
+    case "3":
+    case "4":
+    case "5":
+        case "5":
+            case "5":
+                case "5":
+                    case "5":
+    case "5":
+     case "5":
+    default: 
+         mensajestadocivil = "Estado civil no reconocido.";
+}
+ 
+alert(`Hola ${nombre}, tu estado civil es: ${Estado}`);
